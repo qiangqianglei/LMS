702f986237fef2e95f9f7a1237cf1b2c197285d0
lqq213@126.com <lqq213@126.com>
2024-09-12 09:42:56 +0800

    提交部分Demo

---
+++ src/main/java/com/lms/demo/leecode/Demo.java
@@ -0,0 +1,33 @@
+package com.lms.demo.leecode;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+    }
+
+    /* 在数组的索引 index 处插入元素 num */
+    void insert(int[] nums, int num, int index) {
+        // 把索引 index 以及之后的所有元素向后移动一位
+        for (int i = nums.length - 1; i > index; i--) {
+            nums[i] = nums[i - 1];
+        }
+        // 将 num 赋给 index 处的元素
+        nums[index] = num;
+    }
+
+    /* 删除索引 index 处的元素 */
+    void remove(int[] nums, int index) {
+        // 把索引 index 之后的所有元素向前移动一位
+        for (int i = index; i < nums.length - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+    }
+
+    /* 链表节点类 */
+    class ListNode {
+        int val;        // 节点值
+        ListNode next;  // 指向下一节点的引用
+        ListNode(int x) { val = x; }  // 构造函数
+    }
+}
---
+++ src/main/java/com/lms/demo/leecode/Demo70.java
@@ -0,0 +1,27 @@
+package com.lms.demo.leecode;
+
+public class Demo70 {
+
+    /**
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     *
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     */
+
+    /**
+     * 1、递归解法
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs1(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs1(n - 1) + climbStairs1(n - 2);
+    }
+
+}
